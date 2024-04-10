c127dd15db98a32ef3f712ee49c686bcaf70f099
tolerevo <tolerevo@hotmail.com>
2024-04-10 05:51:34 -0600

    add contact picker module

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <select
+      onChange={onChange}
+      value={value}
+      name={name}
+      aria-label="Contact Picker"
+    >
+      <option value={""} key={-1}>
+        "No Contact Selected"
+      </option>
+      {contacts.map((contact) => {
+        return (
+          <option value={contact} key={contact}>
+            {contact}
+          </option>
+        );
+      })}
+    </select>
   );
 };
