720595703b0ea5a7896d1908e1e4deb976f5a30f
tolerevo <tolerevo@hotmail.com>
2024-04-10 02:52:27 -0600

    add Tile List module

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Tile } from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {
+        tiles.map((tile, index) => {
+          const { name, ...description } = tile;
+          return <Tile name={name} key={index} description={description} />;
+        })
+      }
     </div>
   );
 };
