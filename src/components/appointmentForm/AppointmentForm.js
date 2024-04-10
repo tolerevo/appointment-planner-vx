5662366fd03decdd3acd70bf79a55226d0a364a8
tolerevo <tolerevo@hotmail.com>
2024-04-10 03:54:47 -0600

    mend

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,10 +1,8 @@
 import React, { useMemo } from "react";
-import { ContactPicker} from '../contactPicker/ContactPicker';
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
@@ -18,31 +16,63 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
   const contactNames = useMemo(() => {
-    return contacts.map(contact => contact.name);
+    return contacts.map((contact) => contact.name);
   }, [contacts]);
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-      <input type="text" name="name" value={name} required placeholder="Appointment Name" aria-label="Appointment Name" onChange={(e) => setTitle(e.target.value)} /> 
+        <input
+          type="text"
+          name="name"
+          value={name}
+          required
+          placeholder="Appointment Name"
+          aria-label="Appointment Name"
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </label>
       <br />
       <label>
-      <ContactPicker name="contact" value={contact} contacts={contactNames} onChange={(e) => setContact(e.target.value)} /> 
+        <ContactPicker
+          name="contact"
+          value={contact}
+          contacts={contactNames}
+          onChange={(e) => setContact(e.target.value)}
+        />
       </label>
       <br />
       <label>
-      <input type="date" name="date" value={date} min={getTodayString()} required aria-label="Date Picker" onChange={(e) => setDate(e.target.value)} /> 
+        <input
+          type="date"
+          name="date"
+          value={date}
+          min={getTodayString()}
+          required
+          aria-label="Date Picker"
+          onChange={(e) => setDate(e.target.value)}
+        />
       </label>
       <br />
       <label>
-      <input type="time" name="time" value={time} required aria-label="Time Picker" onChange={(e) => setTime(e.target.value)} />
+        <input
+          type="time"
+          name="time"
+          value={time}
+          required
+          aria-label="Time Picker"
+          onChange={(e) => setTime(e.target.value)}
+        />
       </label>
       <br />
-      <input type="submit" value="Add Appointment" aria-label="Add Appointment" /> 
+      <input
+        type="submit"
+        value="Add Appointment"
+        aria-label="Add Appointment"
+      />
     </form>
   );
 };
--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,24 +7,51 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <form onSubmit={handleSubmit}> 
+    <form onSubmit={handleSubmit}>
       <label>
-      <input type="text" id="name" name="name" value={name} required  placeholder="Contact Name" aria-label="Contact-Name" onChange={(e) => setName(e.target.value)} />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          required
+          placeholder="Contact Name"
+          aria-label="Contact-Name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </label>
       <br />
       <label>
-      <input type="tel" id="phone" name="phone" value={phone} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" required placeholder="Contact Phone" aria-label="Contact Phone" onChange={(e) => setPhone(e.target.value)} />
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={phone}
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          required
+          placeholder="Contact Phone (###-###-####)"
+          aria-label="Contact Phone"
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </label>
       <br />
       <label>
-      <input type="email" id="email" name="email" value={email} required placeholder="Contact Email" aria-label="Contact Email" onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          required
+          placeholder="Contact Email"
+          aria-label="Contact Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </label>
       <br />
       <input type="submit" value="Add Contact" aria-label="Add Contact" />
     </form>
   );
 };
-
