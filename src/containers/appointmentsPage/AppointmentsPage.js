5849d4739a999e0ab206fc8aff40ef647b7d4a0b
tolerevo <tolerevo@hotmail.com>
2024-04-10 03:20:17 -0600

    add appointmentpage module

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,7 +3,15 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({
+  appointments,
+  contacts,
+  addAppointment,
+}) => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
   /*
   Define state variables for 
   appointment info
@@ -11,20 +19,26 @@ export const AppointmentsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    addAppointment(name, contact, date, time);
+    setName("");
+    setContact("");
+    setDate("");
+    setTime("");
     /*
     Add contact info and clear data  
     */
-   
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm contacts={contacts} name={name} setTitle={setName} contact={contact} setContact={setContact} date={date} setDate={setDate} time={time} setTime={setTime} handleSubmit={handleSubmit}/>
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments}/>
       </section>
     </div>
   );
