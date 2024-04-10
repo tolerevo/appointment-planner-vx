ccf446aa0a712e10bf4b9f3b8ee97669cc7efb41
tolerevo <tolerevo@hotmail.com>
2024-04-10 02:33:19 -0600

    complete contactform

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,23 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}> 
+      <label>
+      <input type="text" id="name" name="name" value={name} required  placeholder="Contact Name" aria-label="Contact-Name" onChange={(e) => setName(e.target.value)} />
+      </label>
+      <br />
+      <label>
+      <input type="tel" id="phone" name="phone" value={phone} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" required placeholder="Contact Phone" aria-label="Contact Phone" onChange={(e) => setPhone(e.target.value)} />
+      </label>
+      <br />
+      <label>
+      <input type="email" id="email" name="email" value={email} required placeholder="Contact Email" aria-label="Contact Email" onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <br />
+      <label>
+      <input type="submit" value="Add Contact" aria-label="Add Contact" />
+      </label>
+    </form>
   );
 };
 
