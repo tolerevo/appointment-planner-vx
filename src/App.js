00e384a8aa2a5447d280144a9e09da889383a25b
tolerevo <tolerevo@hotmail.com>
2024-04-09 23:41:08 -0600

    add ContactsPage module

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import React, { useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 import ErrorPage from "./error-page";
 
-
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
   /*
   Define state variables for 
   contacts and appointments 
   */
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
+  const addAppointment = (name, contact, date, time) => {
+    setAppointments([
+      appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
   /*
   Implement functions to add data to
   contacts and appointments
   */
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> } errorElement={ <ErrorPage />} >
-      
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
-    </Route>
-  ));
-  
-  return (
-    <RouterProvider router={router}/>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+        <Route
+          path={ROUTES.CONTACTS}
+          element={
+            <ContactsPage
+              contacts={contacts}
+              addContact={addContact}
+            /> /* Add props to ContactsPage */
+          }
+        />
+        <Route
+          path={ROUTES.APPOINTMENTS}
+          element={
+            <AppointmentsPage
+              contacts={contacts}
+              appointments={appointments}
+              addAppointment={addAppointment}
+            /> /* Add props to AppointmentsPage */
+          }
+        />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
   /*
   Define state variables for 
   contact info and duplicate check
@@ -11,6 +15,13 @@ export const ContactsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!duplicate) {
+      addContact(name, phone, email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
     /*
     Add contact info and clear data
     if the contact name is not a duplicate
@@ -21,15 +32,40 @@ export const ContactsPage = () => {
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
-
+  useEffect(() => {
+    const nameIsDuplicate = () => {
+      const found = contacts.find((contact) => contact.name === name);
+      if (found !== undefined) {
+        return true;
+      }
+      return false;
+    };
+    if (nameIsDuplicate()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate]);
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact {duplicate ? "- Name Already Exists" : ""}</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          duplicate={duplicate}
+          setDuplicate={setDuplicate}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList tiles={contacts} />
       </section>
     </div>
   );
